e7435ed27ea239d432888ddd4d60d58135c0a713
Keith Peters <kp@bit-101.com>
2018-04-07 19:14:29 -0400

    go project

---
+++ go_project/%TINPIG_PROJECT_DIR%.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"${TINPIG_PROJECT_DIR}/hello"
+)
+
+func main() {
+	hello.Hello()
+}
---
+++ go_project/hello/hello.go
@@ -0,0 +1,9 @@
+package hello
+
+import (
+	"fmt"
+)
+
+func Hello() {
+	fmt.Printf("Hello, world!\n")
+}
